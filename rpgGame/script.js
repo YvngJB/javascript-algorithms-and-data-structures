60839081e50fef33ba13987d51781707d7282fa8
YvngJB <jasonb853@googlemail.com>
2024-10-22 21:39:56 +0200

    started building page for Game

--- rpgGame/script.js
+++ rpgGame/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");12
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1")
+const button2 = document.querySelector("#button2")
+const button3 = document.querySelector("#button3")
